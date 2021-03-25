945be1e6cfe942e8b7dc2c1b5f6d28f9b45c90a2
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-03-25 21:15:47 +0900

    Add(jiwon/typescript): logger와 eslint typescript로 변경

--- app/src/app.ts
+++ app/src/app.ts
@@ -2,7 +2,8 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as cookieParser from "cookie-parser";
 import * as dotenv from "dotenv";
-
+import * as morgan from "morgan";
+import logger from "./config/logger"
 
 const app: express.Application = express();
 dotenv.config();
@@ -11,6 +12,7 @@ app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(morgan("tiny", { stream: {write: message => logger.info(message)} }));
 
 import sale from "./apis/sale-list";
 
--- app/src/bin/www.ts
+++ app/src/bin/www.ts
@@ -1,8 +1,8 @@
 import app from "../app";
 import * as express from "express";
+import logger from "../config/logger";
 
-const port : number = Number(process.env.PORT) || 8080;
-const application : express.Application = app;
+const PORT: number = Number(process.env.PORT) || 8080;
+const application: express.Application = app;
 
-application.listen(port, () => console.log(`${port} 포트 오픈`))
-.on(`error`, err => console.error(err));
+application.listen(PORT, () => logger.info(`${PORT} 포트에서 서버가 가동되었습니다.`)).on(`error`, (err) => console.error(err));
